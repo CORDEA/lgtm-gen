9930827465fa813ff1588493b34b669b6376bc0e
Yoshihiro Tanaka <contact@cordea.jp>
2024-04-28 15:13:19 +0900

    add a drop area

---
+++ src/components/DropArea.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+function hasImage(data: DataTransfer | null): boolean {
+  const items = data?.items ?? []
+  if (items.length <= 0) {
+    return false
+  }
+  const item = items[0]
+  if (item.kind !== 'file') {
+    return false
+  }
+  return item.type.startsWith('image');
+
+}
+
+function onDrag(event: DragEvent) {
+  if (hasImage(event.dataTransfer)) {
+    event.preventDefault()
+  }
+}
+
+function onDrop(event: DragEvent) {
+  const data = event.dataTransfer
+  if (!hasImage(data)) {
+    return
+  }
+  event.preventDefault()
+  const item = data!.items[0]
+  const file = item.getAsFile()
+  console.log(file)
+}
+</script>
+
+<template>
+  <div class="card" @dragover="onDrag" @drop="onDrop">
+    <p>Drop the image</p>
+  </div>
+</template>
+
+<style scoped>
+
+.card {
+  width: 80vw;
+  padding: 64px;
+  background: #ffffffa0;
+  border-radius: 24px;
+  border: #ffffff dashed 1px;
+}
+
+</style>
