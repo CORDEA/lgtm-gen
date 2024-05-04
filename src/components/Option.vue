cb7ab53dfe1abd09f95349fa233d1b80feae2f0e
Yoshihiro Tanaka <contact@cordea.jp>
2024-05-04 13:48:51 +0900

    add options

---
+++ src/components/Option.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const text = defineModel('text')
+const color = defineModel('color')
+const size = defineModel('size')
+</script>
+
+<template>
+  <div class='card'>
+    <input type='text' id='text' placeholder="Text" v-model="text"/>
+    <input type='color' id='color' v-model="color"/>
+    <input type='range' id='size' v-model="size"/>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+}
+
+#text {
+  border: 1px solid #fafafa;
+  padding: 12px 16px;
+}
+
+#color {
+  border: 1px solid #fafafa;
+  margin: 16px 0 0 auto;
+}
+
+#size {
+  margin-top: 16px;
+  accent-color: #fafafa;
+}
+
+@media (prefers-color-scheme: light) {
+  #text {
+    border-color: #212121;
+  }
+
+  #color {
+    border-color: #212121;
+  }
+
+  #size {
+    accent-color: #212121;
+  }
+}
+</style>
--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {watch} from "vue";
 
-const props = defineProps(['file'])
+const props = defineProps(['file', 'text', 'color', 'size'])
 
 watch(props, async (next, _) => {
   const canvas = document.getElementById('canvas') as HTMLCanvasElement
@@ -9,17 +9,19 @@ watch(props, async (next, _) => {
   if (!ctx) {
     return;
   }
+  const font = getComputedStyle(canvas, null)
+      .getPropertyValue('font-family')
   const reader = new FileReader();
   const img = new Image();
   img.onload = () => {
     canvas.width = img.width
     canvas.height = img.height
     ctx.drawImage(img, 0, 0)
-    ctx.font = '50px sans-serif'
-    ctx.fillStyle = '#000'
+    ctx.font = `${props.size}px ${font}`
+    ctx.fillStyle = props.color
     ctx.textAlign = 'center'
     ctx.textBaseline = 'middle'
-    ctx.fillText('LGTM', canvas.width / 2, canvas.height / 2, canvas.width)
+    ctx.fillText(props.text, canvas.width / 2, canvas.height / 2, canvas.width)
   }
   reader.onload = (e) => {
     img.src = e.target?.result as string
